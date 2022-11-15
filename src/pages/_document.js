97f2237e251f18b07ab532e5ba5ff59f461071a4
ovvpeva <118193319+ovvpeva@users.noreply.github.com>
2022-11-15 13:59:27 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,11 +15,11 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="HTML5 Template Vie onepage themeforest"
+            content="Intelligent control systems"
           />
           <meta
             name="description"
-            content="Vie - Onepage Multi-Purpose HTML5 Template"
+            content="Intelligent control systems"
           />
           <meta name="author" content="" />
           <link rel="shortcut icon" href="/img/favicon.ico" />
